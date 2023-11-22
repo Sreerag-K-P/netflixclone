0953a7c42033347dce92842846eb221b721cec43
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-11-22 17:23:39 +0530

    search Feature is added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,53 @@
+"use client";
 import Image from "next/image";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
+  const route = useRouter();
+  const [search, setSearch] = useState("");
+
   return (
-    <div className="w-full flex justify-between items-center bg-black px-9 h-12 ">
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
-        alt="NetflixLogo"
-        width={1920}
-        height={40}
-        className="w-28 h-fit "
-      />
-      <Image
-        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-        alt="avatar"
-        width={1920}
-        height={30}
-        className="w-10 h-fit"
-      />
+    <div className="w-full absolute z-30 flex justify-between items-center bg-black/5 px-9 h-16 ">
+      <Link href="/">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+          alt="NetflixLogo"
+          width={1920}
+          height={40}
+          className="w-28 h-fit "
+        />
+      </Link>
+
+      <div className="flex gap-2 items-center">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            route.push(`/${search}`);
+          }}
+          className=" shadow-sm flex-row items-center justify-center"
+        >
+          <input
+            className="text-white font-bold py-1 bg-transparent max-sm:w-32 border-b-[1px] outline-none"
+            type="text"
+            name="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search..."
+          />
+          <button type="submit" className="text-white font-bold text-2xl ">
+            <CiSearch />
+          </button>
+        </form>
+        <Image
+          src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+          alt="avatar"
+          width={1920}
+          height={30}
+          className="w-10 h-fit"
+        />
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Rowposts from "./components/Rowposts";
 
 export default function Home() {
   return (
-    <main className="bg-zinc-900">
-      <Banner />
-      <Rowposts />
-      <Rowposts title="Action" />
-      <Rowposts title="Drama" />
-      <Rowposts title="Sci-Fi" />
+    <main className="bg-zinc-900 ">
+      <div className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <Rowposts />
+        <Rowposts title="Action" />
+        <Rowposts title="Drama" />
+        <Rowposts title="Sci-Fi" />
+      </div>
     </main>
   );
 }
