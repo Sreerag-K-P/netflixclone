26936a370e47c99956fcf409d1c5f453c6ecee7b
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-11-22 12:46:15 +0530

    created rowposts like where we have different movies poster and can play the exact video when you click the poster

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Banner from "./components/Banner";
+import Rowposts from "./components/Rowposts";
 
 export default function Home() {
   return (
     <main className="bg-zinc-900">
       <Banner />
+      <Rowposts />
+      <Rowposts title="Action" />
+      <Rowposts title="Drama" />
+      <Rowposts title="Sci-Fi" />
     </main>
   );
 }
