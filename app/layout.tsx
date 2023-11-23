bedb4713268431a277ebe23f1940d3191b7aa5dd
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-11-23 13:34:35 +0530

    netflix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Netfix",
+  title: "Netflix",
   description: "created by sreerag k p",
 };
 
